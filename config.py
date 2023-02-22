7469797137a8e6f4e1b19ff5d6960118e23ce30b
IgorCattusso <igor_catusso@hotmail.com>
2023-02-22 17:08:08 -0300

    Refactoring keys and get_tickets function

--- config.py
+++ config.py
@@ -3,13 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
-
 API_BASE_URL = "https://agilepromoter1671733243.zendesk.com/"
 
 USERNAME = "igor.cattusso@involves.com"
 
-ZENDESK_API_KEY = SECRET_KEY
+ZENDESK_API_KEY = str(os.getenv('SECRET_KEY'))
 
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{user}:{password}@{server}/{database}'.format(
--- views.py
+++ views.py
@@ -9,8 +9,8 @@ from models import zendesk_tickets
 @app.route("/get-tickets")
 def get_tickets():
     zendesk_endpoint_url = "api/v2/search.json"
-    zendesk_search_query = "?query=type:ticket status:new"
-    api_url = API_BASE_URL + zendesk_endpoint_url + zendesk_search_query
+    zendesk_search_query = "query=type:ticket status:new"
+    api_url = API_BASE_URL + zendesk_endpoint_url + "?" + zendesk_search_query
 
     api_response = requests.get(api_url, headers=generate_zendesk_headers())
 
