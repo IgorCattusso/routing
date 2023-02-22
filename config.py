4c42a4c9f47aba8eb1e9d09aa43148ff34bc61e0
Igor Cattusso <igor_catusso@hotmail.com>
2023-02-22 09:31:40 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ API_BASE_URL = "https://agilepromoter1671733243.zendesk.com/"
 
 USERNAME = "igor.cattusso@involves.com"
 
-ZENDESK_API_KEY = "dcAxT2yYGwZWmPIkUJuwZotJdwFAgn3GxzLBajPE"
+ZENDESK_API_KEY = ""
 
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{user}:{password}@{server}/{database}'.format(
