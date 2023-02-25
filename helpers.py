695d2ea4320fd8614fd429cdcde7c89e51fea700
IgorCattusso <igor_catusso@hotmail.com>
2023-02-25 20:14:20 -0300

    Fixed function to generate json for assigning ticket

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
+from sqlalchemy import *
 from config import *
 import base64
 from models import *
+from app import db
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
 
 
 def generate_zendesk_headers():
@@ -27,18 +31,27 @@ def match_false_true(value):
 
 
 def zendesk_default_user_group(zendesk_user_id):
-    user_default_group = ZendeskGroupMemberships.query.filter_by(zendesk_user_id=zendesk_user_id, default=1).first()
+    result = db.session.execute(select(ZendeskGroupMemberships.group_id).filter_by(
+        zendesk_user_id=zendesk_user_id, default=1)).scalar_one()
 
-    return user_default_group
+    return result
 
 
 def generate_assign_tickets_json(zendesk_user_id):
-    json = '[ { \
-      "ticket": { \
-        "status": "open", \
-        "assignee_id": {}, \
-        "group_id": {} \
-      } \
-    } ]'.format(zendesk_user_id, zendesk_default_user_group(zendesk_user_id))
+    try:
+        json = \
+            {
+                "ticket": {
+                    "status": "open",
+                    "assignee_id": zendesk_user_id,
+                    "group_id": zendesk_default_user_group(zendesk_user_id)
+                }
+            }
 
-    return json
+        return json
+
+    except NoResultFound:
+        return 'Usuário não possui grupo padrão!'
+
+    except MultipleResultsFound:
+        return 'Usuário possui mais de um registro de grupo padrão!'
