8dafff59da4d8add1686d6278c2ca3e98508969a
IgorCattusso <igor_catusso@hotmail.com>
2023-03-11 19:40:09 -0300

    refactor: removed unnecessary function

--- helpers.py
+++ helpers.py
@@ -1,12 +1,11 @@
 import base64
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from helpers import *
 from config import *
 from models import *
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 engine = create_engine(url_object)
+
 
 def generate_zendesk_headers():
     concatenate = USERNAME + '/token:' + ZENDESK_API_KEY
@@ -50,16 +49,7 @@ def generate_assign_tickets_json(zendesk_user_id):
         return 'Usuário possui mais de um registro de grupo padrão!'
 
 
-def get_zendesk_users_id(user_id):
-    stmt = select(ZendeskUsers.id).where(ZendeskUsers.zendesk_user_id == user_id)
-    with Session(engine) as session:
-        db_user_id = session.execute(stmt)
-        if db_user_id:
-            return db_user_id.first()[0]
-
 class CustomFields:
     def __init__(self, field_id, value):
         self.field_id = field_id
         self.value = value
-
-
