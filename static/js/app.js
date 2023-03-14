687fd14eb4043a6665b66ec00ef1b0cc036ef37f
IgorCattusso <igor_catusso@hotmail.com>
2023-03-13 22:40:17 -0300

    feat!: better visual interface and navigation

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
-const button = document.getElementById("seeUser");
+const button = document.getElementById("edit");
 button.addEventListener("click", function() {
   // get selected row IDs
-  const table = document.getElementById("groupsTable");
+  const table = document.getElementById("contentTable");
   const rows = table.getElementsByTagName("tr");
   const selectedRowIDs = [];
   for (let i = 0; i < rows.length; i++) {
@@ -18,8 +18,10 @@ button.addEventListener("click", function() {
   }
 });
 
-const table = document.getElementById("groupsTable");
-const rows = table.getElementsByTagName("tr");
+
+const table = document.getElementById("contentTable");
+const header = table.querySelector("thead");
+const rows = table.querySelectorAll("tbody tr");
 for (let i = 0; i < rows.length; i++) {
   rows[i].addEventListener("click", function() {
     // toggle "selected" class on clicked row
@@ -32,4 +34,56 @@ for (let i = 0; i < rows.length; i++) {
       button.setAttribute("disabled", true);
     }
   });
+}
+
+
+// Add click event listener to each table header
+header.querySelectorAll("th").forEach((th, i) => {
+  th.addEventListener("click", () => {
+    // Sort table rows based on the clicked header
+    const sortedRows = Array.from(rows);
+    const order = (th.dataset.order || "asc") === "asc" ? "desc" : "asc";
+    const sortFunction = getSortFunction(i, order);
+    sortedRows.sort(sortFunction);
+
+    // Reorder the table rows
+    const tbody = table.querySelector("tbody");
+    tbody.innerHTML = "";
+    sortedRows.forEach((row) => {
+      tbody.appendChild(row);
+    });
+
+    // Update the header to reflect the sort order
+    header.querySelectorAll("th").forEach((th2) => {
+      th2.dataset.order = "";
+    });
+    th.dataset.order = order;
+  });
+});
+
+
+// Helper function to get the sort function for a specific column and order
+function getSortFunction(column, order) {
+  return (a, b) => {
+    const aVal = a.querySelectorAll("td")[column].textContent.trim();
+    const bVal = b.querySelectorAll("td")[column].textContent.trim();
+    if (aVal === bVal) {
+      return 0;
+    }
+    if (order === "asc") {
+      return aVal < bVal ? -1 : 1;
+    } else {
+      return aVal > bVal ? -1 : 1;
+    }
+  };
+}
+
+
+function toggleOptions() {
+  var options = document.getElementById("options");
+  if (options.style.display === "none") {
+    options.style.display = "block";
+  } else {
+    options.style.display = "none";
+  }
 }
