fe529e015c8c43ef4abf7882b738f351043b068d
IgorCattusso <igor_catusso@hotmail.com>
2023-03-15 07:53:29 -0300

    refactor: visual interface and navigation overhaul

--- static/js/app.js
+++ static/js/app.js
@@ -87,3 +87,35 @@ function toggleOptions() {
     options.style.display = "none";
   }
 }
+
+
+
+$(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var dropdownlink = this.el.find('.dropdown-link');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the ul.submenuItems
+        $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.submenu-Items').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.accordion-menu'), false);
+})
