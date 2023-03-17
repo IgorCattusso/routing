1b0424f325cb00312abc3dfeb7bfeb989ee50ba3
IgorCattusso <igor_catusso@hotmail.com>
2023-03-16 21:56:44 -0300

    refactor: divided routes into separate files for better organization

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 from views import *
+from views_users import *
+from views_groups import *
+from views_tickets import *
 
 engine = create_engine(url_object)
 
