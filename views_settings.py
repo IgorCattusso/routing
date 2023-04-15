240404f60a2a2fa7e988d26601beb4166475d754
IgorCattusso <igor_catusso@hotmail.com>
2023-04-14 22:36:55 -0300

    feat!: added the Zendesk Schedules objects

--- views_settings.py
+++ views_settings.py
@@ -5,8 +5,9 @@ from app import app
 from models import *
 from sqlalchemy import create_engine, select, case, desc
 from sqlalchemy.orm import Session
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 import time
+from datetime import datetime, timedelta
 
 engine = create_engine(url_object)
 
@@ -140,10 +141,10 @@ def get_ticket_forms():
             for ticket_fields_in_form in form['ticket_field_ids']:
                 stmt = \
                     select(ZendeskTicketFieldsInForms) \
-                    .join(ZendeskTicketForms) \
-                    .join(ZendeskTicketFields) \
-                    .where(ZendeskTicketForms.zendesk_ticket_form_id == form_id) \
-                    .where(ZendeskTicketFields.zendesk_ticket_field_id == ticket_fields_in_form)
+                        .join(ZendeskTicketForms) \
+                        .join(ZendeskTicketFields) \
+                        .where(ZendeskTicketForms.zendesk_ticket_form_id == form_id) \
+                        .where(ZendeskTicketFields.zendesk_ticket_field_id == ticket_fields_in_form)
 
                 with Session(engine) as session:
                     query_result = session.execute(stmt).first()
@@ -177,8 +178,8 @@ def get_ticket_forms():
     inserted_ticket_field_options = []
 
     stmt = select(ZendeskTicketFields.zendesk_ticket_field_id) \
-          .join(ZendeskTicketFieldsInForms) \
-          .join(ZendeskTicketForms, ZendeskTicketForms.id == ZendeskTicketFieldsInForms.zendesk_ticket_forms_id)
+        .join(ZendeskTicketFieldsInForms) \
+        .join(ZendeskTicketForms, ZendeskTicketForms.id == ZendeskTicketFieldsInForms.zendesk_ticket_forms_id)
     zendesk_ticket_fields = session.execute(stmt)
 
     for field in zendesk_ticket_fields:
@@ -193,7 +194,7 @@ def get_ticket_forms():
             while next_url:
                 for option in api_response['custom_field_options']:
                     stmt = select(ZendeskTicketFieldOptions) \
-                          .where(ZendeskTicketFieldOptions.zendesk_ticket_field_option_id == option['id'])
+                        .where(ZendeskTicketFieldOptions.zendesk_ticket_field_option_id == option['id'])
                     with Session(engine) as session:
                         query_result = session.execute(stmt).first()
                         if not query_result:
@@ -217,7 +218,7 @@ def get_ticket_forms():
                     api_response = requests.get(next_url, headers=generate_zendesk_headers()).json()
 
     if inserted_ticket_fields or inserted_ticket_forms or \
-       inserted_ticket_fields_in_forms or inserted_ticket_field_options:
+            inserted_ticket_fields_in_forms or inserted_ticket_field_options:
         flash(f'Ticket Fields inseridos: {str(inserted_ticket_fields)};'
               f'Ticket Forms inseridos: {str(inserted_ticket_forms)};'
               f'Ticket Fields in Forms inseridos: {str(inserted_ticket_fields_in_forms)};'
@@ -263,3 +264,99 @@ def get_tags():
     else:
         flash(f'Nenhuma tag inserida!')
         return redirect(url_for('tags'))
+
+
+@app.route('/routing-settings', methods=['PUT'])
+def update_general_settings():
+    if request.method == 'PUT':
+        data = request.get_json()
+
+        if data['agent_backlog_limit'] == '':
+            agent_backlog_limit = None
+        else:
+            agent_backlog_limit = data['agent_backlog_limit']
+
+        if data['daily_assignment_limit'] == '':
+            daily_assignment_limit = None
+        else:
+            daily_assignment_limit = data['daily_assignment_limit']
+
+        if data['hourly_assignment_limit'] == '':
+            hourly_assignment_limit = None
+        else:
+            hourly_assignment_limit = data['hourly_assignment_limit']
+
+        with Session(engine) as session:
+            GeneralSettings.update_settings(
+                session,
+                data['use_routes'],
+                data['routing_model'],
+                agent_backlog_limit,
+                daily_assignment_limit,
+                hourly_assignment_limit,
+            )
+            session.commit()
+
+    return 'Data processed successfully'
+
+
+@app.route('/get-schedules')
+def get_schedules():
+    zendesk_endpoint_url = '/api/v2/business_hours/schedules'
+    api_url = API_BASE_URL + zendesk_endpoint_url
+
+    inserted_schedules = []
+
+    api_response = requests.get(api_url, headers=generate_zendesk_headers()).json()
+
+    for schedule in api_response['schedules']:
+        with Session(engine) as session:
+            if not ZendeskSchedules.check_for_existing_schedule(session, schedule['id']):
+                ZendeskSchedules.insert_schedule(session, schedule['id'], schedule['name'], schedule['time_zone'])
+                session.commit()
+                inserted_schedules.append(schedule['id'])
+
+            for interval in schedule['intervals']:
+                schedule_id = ZendeskSchedules.get_id_from_zendesk_schedule_id(session, schedule['id'])
+                ZendeskSchedules.new_insert_schedule_hour(
+                    session,
+                    schedule_id,
+                    interval['start_time'],
+                    interval['end_time']
+                )
+                session.commit()
+
+    time.sleep(.35)
+
+    if inserted_schedules:
+        flash(f'Programações inseridas: {str(inserted_schedules)}')
+        return redirect(url_for('schedules'))
+    else:
+        flash(f'Nenhuma Programação inserida!')
+        return redirect(url_for('schedules'))
+
+
+@app.route('/schedule/<int:schedule_id>')
+def get_schedule_hours(schedule_id):
+    with Session(engine) as session:
+        schedule = ZendeskSchedules.get_schedule(session, schedule_id)
+
+    time.sleep(.35)
+
+    times_list = []
+
+    for item in schedule:
+        if type(item) == timedelta:
+            dt = datetime(1, 1, 1, 0, 0) + item
+            times_list.append(dt.strftime("%H:%M:%S"))
+        elif not item:
+            times_list.append('--:--:--')
+
+    return render_template(
+        'schedule.html',
+        schedule_id=schedule.id,
+        schedule_name=schedule.name,
+        schedule_timezone=schedule.timezone,
+        schedule_times=times_list,
+        schedule_times_len=len(times_list),
+    )
