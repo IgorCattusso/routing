5e10ed068a3b0dc114588a5fbe32abc7a2b4eb88
IgorCattusso <igor_catusso@hotmail.com>
2023-04-29 17:18:55 -0300

    fix/refactor: changed the database session object variable name and fixed minor bugs to better accommodate new the new ticket assignment process

--- views_zendesk_tickets.py
+++ views_zendesk_tickets.py
@@ -1,7 +1,7 @@
 from config import API_BASE_URL, ZENDESK_TICKET_LEVEL_ID
 import requests
-from models import ZendeskTickets, ZendeskUsers, ZendeskUserBacklog
-from helpers import generate_zendesk_headers, generate_assign_tickets_json
+from models import ZendeskTickets, ZendeskUsers, UserBacklog, Users
+from helpers import generate_zendesk_headers
 from app import app, engine
 from sqlalchemy import select, update, and_, or_, delete
 from sqlalchemy.orm import Session
@@ -25,10 +25,12 @@ def get_tickets_to_be_assigned():
             with Session(engine) as session:
                 existing_ticket = session.execute(stmt).first()
                 if not existing_ticket:
-                    new_ticket = ZendeskTickets(ticket_id=ticket['id'], channel=ticket['via']['channel'],
-                                                subject=ticket['subject'],
-                                                created_at=ticket['created_at'].replace('T', ' ').replace('Z', ''),
-                                                )
+                    new_ticket = ZendeskTickets(
+                        ticket_id=ticket['id'],
+                        channel=ticket['via']['channel'],
+                        subject=ticket['subject'],
+                        created_at=ticket['created_at'].replace('T', ' ').replace('Z', ''),
+                    )
 
                     inserted_tickets.append(ticket['id'])
 
@@ -65,11 +67,12 @@ def get_user_backlog():
     next_url = api_url
 
     while next_url:
-        with Session(engine) as session:
+        with Session(engine) as db_session:
             for ticket in api_response['results']:
                 if ticket['assignee_id']:
-                    zendesk_users_id = ZendeskUsers.get_zendesk_users_id(ticket['assignee_id'])
-                    if zendesk_users_id != 'null':
+                    zendesk_users_id = ZendeskUsers.get_zendesk_users_id(db_session, ticket['assignee_id'])
+                    users_id = Users.get_user_from_zendesk_users_id(db_session, zendesk_users_id)
+                    if users_id != 'null':
                         ticket_id = int(ticket['id'])
                         ticket_status = str(ticket['status'])
                         custom_fields = ticket['custom_fields']
@@ -79,8 +82,8 @@ def get_user_backlog():
                                 if ticket_level == 'None':
                                     ticket_level = ''
 
-                        new_user_backlog = ZendeskUserBacklog(
-                            zendesk_users_id=zendesk_users_id,
+                        new_user_backlog = UserBacklog(
+                            users_id=users_id,
                             ticket_id=ticket_id,
                             ticket_status=ticket_status,
                             ticket_level=ticket_level,
@@ -89,56 +92,56 @@ def get_user_backlog():
                         '''
                         Verificando se o ticket existe exatamente igual
                         '''
-                        stmt = select(ZendeskUserBacklog.id) \
-                            .where(ZendeskUserBacklog.ticket_id == new_user_backlog.ticket_id) \
-                            .where(ZendeskUserBacklog.zendesk_users_id == new_user_backlog.zendesk_users_id) \
-                            .where(ZendeskUserBacklog.ticket_status == new_user_backlog.ticket_status) \
-                            .where(ZendeskUserBacklog.ticket_level == new_user_backlog.ticket_level)
-                        existing_backlog = session.execute(stmt).first()
+                        stmt = select(UserBacklog.id) \
+                            .where(UserBacklog.ticket_id == new_user_backlog.ticket_id) \
+                            .where(UserBacklog.users_id == new_user_backlog.users_id) \
+                            .where(UserBacklog.ticket_status == new_user_backlog.ticket_status) \
+                            .where(UserBacklog.ticket_level == new_user_backlog.ticket_level)
+                        existing_backlog = db_session.execute(stmt).first()
                         if not existing_backlog:  # Se o ticket exatamente igual não existe, então atualizar ou inserir
                             '''
                             Verificando se o ticket existe, mas tem coisa diferente, então precisa atualizar
                             '''
-                            stmt = select(ZendeskUserBacklog.id) \
+                            stmt = select(UserBacklog.id) \
                                 .where(
                                 and_(
-                                    ZendeskUserBacklog.ticket_id == new_user_backlog.ticket_id
+                                    UserBacklog.ticket_id == new_user_backlog.ticket_id
                                 ),
                                 or_(
-                                    ZendeskUserBacklog.zendesk_users_id == new_user_backlog.zendesk_users_id,
-                                    ZendeskUserBacklog.ticket_status == new_user_backlog.ticket_status,
-                                    ZendeskUserBacklog.ticket_level == new_user_backlog.ticket_level
+                                    UserBacklog.users_id == new_user_backlog.users_id,
+                                    UserBacklog.ticket_status == new_user_backlog.ticket_status,
+                                    UserBacklog.ticket_level == new_user_backlog.ticket_level
                                 )
                             )
-                            existing_ticket_id = session.execute(stmt).scalar()  # scalar() pra retornar só o ticket_id
+                            existing_ticket_id = db_session.execute(stmt).scalar()  # scalar() pra retornar só o ticket_id
                             if existing_ticket_id:  # Se retornou o ID do ticket, então atualizar as informações
-                                session.execute(
-                                    update(ZendeskUserBacklog).where(ZendeskUserBacklog.id == existing_ticket_id)
-                                    .values(zendesk_users_id=new_user_backlog.zendesk_users_id,
+                                db_session.execute(
+                                    update(UserBacklog).where(UserBacklog.id == existing_ticket_id)
+                                    .values(zendesk_users_id=new_user_backlog.users_id,
                                             ticket_status=new_user_backlog.ticket_status,
                                             ticket_level=new_user_backlog.ticket_level,
                                             )
                                 )
-                                session.commit()
+                                db_session.commit()
                                 updated_backlog.append(new_user_backlog.ticket_id)
                             else:  # Se não retornou o ID do ticket, então precisa inserir
-                                session.add(new_user_backlog)
-                                session.commit()
+                                db_session.add(new_user_backlog)
+                                db_session.commit()
                                 inserted_backlog.append(new_user_backlog.ticket_id)
 
                 all_tickets_in_api_return.append(ticket['id'])
 
-            stmt = select(ZendeskUserBacklog.ticket_id)
-            for row in session.execute(stmt).all():
+            stmt = select(UserBacklog.ticket_id)
+            for row in db_session.execute(stmt).all():
                 all_tickets_in_database_backlog.append(row[0])
 
             tickets_to_be_removed_from_backlog = \
                 [x for x in all_tickets_in_database_backlog if x not in all_tickets_in_api_return]
 
             for ticket_to_be_removed in tickets_to_be_removed_from_backlog:
-                delete_stmt = delete(ZendeskUserBacklog).where(ZendeskUserBacklog.ticket_id == ticket_to_be_removed)
-                session.execute(delete_stmt)
-                session.commit()
+                delete_stmt = delete(UserBacklog).where(UserBacklog.ticket_id == ticket_to_be_removed)
+                db_session.execute(delete_stmt)
+                db_session.commit()
                 deleted_backlog.append(ticket_to_be_removed)
 
         next_url = api_response['next_page']
