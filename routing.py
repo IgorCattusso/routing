67ea460bf87bed6b7ada97f726ba8ee5ae069fd1
IgorCattusso <igor_catusso@hotmail.com>
2023-04-29 17:37:17 -0300

    fix: added conditions to only continue validating rules if the next user is valid

--- routing.py
+++ routing.py
@@ -53,57 +53,65 @@ def assign_ticket():
 
     assigned_ticket = False
 
-    if next_ticket:
-        while not assigned_ticket:
+    while not assigned_ticket:
+        if next_ticket and recipient_user_for_ticket and user and settings:
             with Session(engine) as db_session:
                 if user.deleted:  # if the user is deleted, delete it from the queue and get the next user in queue
                     print('if 1')
                     UsersQueue.delete_user_from_queue(db_session, user.id)
                     db_session.commit()
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not user.active or user.routing_status == 0:  # if the user is not active or offline, remove it
                     print('if 2')                                  # from the queue and get the next user in queue
                     UsersQueue.remove_user_from_queue(db_session, user.id)
                     db_session.commit()
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif user.routing_status == 2:  # if the user is away, get the next user in the queue
                     print('if 3 - OK')
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not UserBacklog.get_agent_backlog_count(db_session, user.id) < settings.agent_backlog_limit:
                     # if the user has more tickets than the backlog limit, move the user
                     # to the end of the queue and get the next user
                     print('if 4')
-                    next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
                     UsersQueue.move_user_to_queue_end(db_session, user.id)
                     db_session.commit()
-                    user = Users.get_user(db_session, next_user.users_id)
+                    next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not AssignedTickets.get_user_assigned_ticket_count_on_the_last_hour(db_session, user.id) \
                         < settings.hourly_assignment_limit:
-                    print('if 5')
+                    print('if 5 - OK')
                     UsersQueue.move_user_to_queue_end(db_session, user.id)
                     db_session.commit()
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not AssignedTickets.get_user_assigned_ticket_count_at_today(db_session, user.id) \
                         < settings.daily_assignment_limit:
                     print('if 6')
                     UsersQueue.remove_user_from_queue(db_session, user.id)
                     db_session.commit()
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not Users.is_user_on_working_hours(db_session, user.id):
                     print('if 7 - OK')
                     UsersQueue.remove_user_from_queue(db_session, user.id)
                     db_session.commit()
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not next_ticket.tag_pais == 'pais_brasil' and user.latam_user == 0:
                     print('if 8')
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif not next_ticket.tag_pais == 'pais_brasil' and user.latam_user == 1 or user.latam_user == 2:
                     print('if 9')
                     assign_ticket_on_zendesk(next_ticket.ticket_id, user.id)
@@ -115,7 +123,8 @@ def assign_ticket():
                 elif next_ticket.tag_pais == 'pais_brasil' and user.latam_user == 1:
                     print('if 10')
                     next_user = UsersQueue.get_next_user_in_queue(db_session, user.id)
-                    user = Users.get_user(db_session, next_user.users_id)
+                    if next_user:
+                        user = Users.get_user(db_session, next_user.users_id)
                 elif next_ticket.tag_pais == 'pais_brasil' and user.latam_user == 0 or user.latam_user == 2:
                     print('if 11')
                     assign_ticket_on_zendesk(next_ticket.ticket_id, user.id)
@@ -124,8 +133,9 @@ def assign_ticket():
                     db_session.commit()
                     assigned_ticket = True
                     return 'success'
-
-        return 'Não há nenhum agente disponível para receber o ticket'
+            return 'Não há nenhum agente disponível para receber o ticket'
+        else:
+            break
 
     return 'Não há nenhum ticket para ser distribuído'
 
